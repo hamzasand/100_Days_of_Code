9e9b79f5b96e88b4f0aa96d2480bc290218b5751
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2025-10-29 11:18:58 +0500

    Update weight_calculator.py

--- weight_calculator.py
+++ weight_calculator.py
@@ -3,7 +3,7 @@ height = 4.87
 #logic to calculate bmi
 bmi = weight /(height ** 2)
 
-if bmi < 18.5:
+if bmi < 18.5: # 18.5 not included
     print("underweight")
 elif 18.5 <= bmi < 25 :
     print("normal weight")
