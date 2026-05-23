947c5f38e734e0eb849dae20da20ef9a2df793e0
hamzasand <mhamzasaeed762@gmail.com>
2026-05-23 18:09:22 +0500

    error resolven

--- file_handling.py
+++ file_handling.py
@@ -5,7 +5,7 @@ def readfileandfolder():
     path = Path('')
     items = list(path.rglob('*'))
     for i , items in enumerate(items):
-        print(f"[i+1] : {items}")
+        print(f"{i+1} : {items}")
 
 
 def createfile():
