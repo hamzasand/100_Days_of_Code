84ee3fb473e282bef197d5290d9af11d8473a084
hamzasand <mhamzasaeed762@gmail.com>
2026-06-01 20:39:25 +0500

    encapulation add

--- encapsulation.py
+++ encapsulation.py
@@ -1,7 +1,11 @@
 class Animal:
-    a = "a attribute from parent class"
+    a = "a attribute from parent class with public variable"
+    _a = "a attribute from parent class with protected variable"
+    __a = "a attribute from parent class with private variable"
     def show(self):
         print("a message from parent class")
 
 obj = Animal()
 print(obj.a)
+print(obj._a)
+print(obj.__a)
