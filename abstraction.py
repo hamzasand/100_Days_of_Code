1c5db7e2a4d1a61991bd5ed9127da397ad9b3688
hamzasand <mhamzasaeed762@gmail.com>
2026-06-01 20:47:22 +0500

    abstraction add

---
+++ abstraction.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Abstract(ABC):
+    @abstractmethod
+    def parameter(self):
+        pass
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Square(Abstract):
+    def __init__(self,side):
+        self.side = side
+
+    def parameter(self):
+        print("ihave created parameters")
+
+    def area(self):
+        print("i have calculate the area")
+  
+obj = Square(7)
